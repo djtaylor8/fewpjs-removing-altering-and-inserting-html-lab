f0011fc8d39fff10ed99b9629a98b39bbb73124c
DJ Taylor <dtaylor88@gmail.com>
2021-08-24 16:52:13 -0700

    commit tree 0834e7376fa7d2a5e43b2c3b30036254f37976e0

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+document.querySelector('#main').remove()
+
+let element = document.createElement('div')
+
+document.body.appendChild(element)
+
+let newHeader = document.createElement('h1')
+
+newHeader.id = "victory" 
+
+newHeader.innerHTML = "DJ is the champion"
